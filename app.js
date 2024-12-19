09c77463b171325b1306694b65c27c8b2923268e
SURESH MANDAL <sureshmandal7989@gmail.com>
2024-12-19 13:51:46 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,60 +1,10 @@
-// const { john, age } = person;
-// console.log(name); 
-// console.log(age); 
+import { add, subtract } from './math.js';
 
+const num1 = 10;
+const num2 = 5;
 
+console.log(add(num1, num2));
+console.log(subtract(num1, num2));
 
-// let a = 10, b = 20;
-// [a, b] = [b, a];
-// console.log(a); 
-// console.log(b); 
-
-
-// const user = {
-//     details: {
-//         name: 'Alice',
-//         age: 25,
-//         address: {
-//             city: 'New York',
-//             country: 'USA'
-//         }
-//     }
-// };
-
-// const {
-//     details: {
-//         name,
-//         age,
-//         address: { city }
-//     }
-// } = user;
-
-// console.log(name); 
-// console.log(age);  
-// console.log(city);
-
-
-// const users = [
-//     { name: 'John', age: 30 },
-//     { name: 'Alice', age: 25 },
-//     { name: 'Bob', age: 35 }
-// ];
-
-// const names = users.map(({ name }) => name);
-
-// console.log(names);
-
-
-
-// function getUserInfo({ name, address: { city = 'Unknown' } }) {
-//     return { name, city };
-// }
-
-// const user = {
-//     name: 'John',
-//     address: {
-//         country: 'USA'
-//     }
-// };
-
-// console.log(getUserInfo(user)); 
+import {name} from "./greeting.js"
+console.log(name)
